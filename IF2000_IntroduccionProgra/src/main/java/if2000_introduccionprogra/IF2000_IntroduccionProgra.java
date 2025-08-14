6175a6650dd6cab43b54fe6245ae59c1bc751f95
NyxRC <riosdanielcc@gmail.com>
2025-08-14 11:20:33 -0600

    Mi Primer Commit en Java

---
+++ IF2000_IntroduccionProgra/src/main/java/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,38 @@
+package if2000_introduccionprogra;
+
+/**
+ *
+ * @author Daniel Ríos
+ */
+public class IF2000_IntroduccionProgra {
+
+    public static void main(String[] args) {
+        
+        //Data types
+        String name = "Leonel";
+        int age = 40;
+        
+        System.out.println("Hello World!");
+        System.out.println("Name"+name);
+        System.out.println("Age"+age);
+        /*
+        Tipos de datos numéricos
+        int, long, short, float, double
+        numeric, money, ddecimal
+        
+        Fechas:
+        Date, Calendar
+        
+        Cadenas carácter:
+        String
+        
+        Objetos:
+        Object
+        
+        Data Structures
+        Arrays or Vectors: int [] ;
+                            int [][] y;
+        */
+        
+    }
+}
