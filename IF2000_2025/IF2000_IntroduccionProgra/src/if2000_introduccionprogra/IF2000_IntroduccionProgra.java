198a7b89c3fca5e0926da3d68cbe45d610e06e39
NyxRC <riosdanielcc@gmail.com>
2025-08-28 20:56:15 -0600

    Mi primer commit en java

---
+++ IF2000_2025/IF2000_IntroduccionProgra/src/if2000_introduccionprogra/IF2000_IntroduccionProgra.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package if2000_introduccionprogra;
+
+/**
+ *
+ * @author Daniel Ríos
+ */
+public class IF2000_IntroduccionProgra {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ IF2000_2025/IF2000_IntroduccionProgra/src/logic/Ejercicios_Semana2_PracticaEnClase.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package logic;
+
+/**
+ *
+ * @author Daniel Ríos
+ */
+public class Ejercicios_Semana2_PracticaEnClase {
+    
+}
